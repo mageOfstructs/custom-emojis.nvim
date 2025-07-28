b3e1d3d79d69b2288e59820e40c791be6e227a60
mageOfStructs <jason.puschnig@gmx.at>
2025-07-28 02:13:34 +0200

    refactor and support multiple windows

--- lua/emoji.lua
+++ lua/emoji.lua
@@ -28,6 +28,39 @@ end
 
 -- :drgn_0_0_256:
 
+local function render_window(win_id, opts)
+	local buf_id = vim.api.nvim_win_get_buf(win_id)
+	local conceal_cmd = 'syntax match myConceal "' .. opts.emoji_regex .. '" conceal'
+	vim.cmd(conceal_cmd)
+	local first_line = vim.fn.winsaveview().topline
+	local matches =
+		vim.fn.matchbufline(buf_id, opts.emoji_regex, first_line, first_line + vim.api.nvim_win_get_height(0))
+	-- print(vim.inspect(matches))
+	for _, match in ipairs(matches) do
+		local text = match.text
+		if string.gmatch(text, "^drgn.*$") then
+			local path = opts.emoji_path .. string.sub(text, 2, #text - 1) .. ".png"
+			-- print(path .. " exists!")
+			local abs_pos = vim.fn.screenpos(win_id, match.lnum, match.byteidx)
+			move_cursor(abs_pos.row, abs_pos.col)
+			M.show_image(0, 0, path)
+			restore_cursor()
+			-- print("Image sent!")
+		end
+	end
+end
+
+local function extract_window_ids(list, ret)
+	for i = 1, #list do
+		local v = list[i]
+		if type(v) == "table" then
+			extract_window_ids(v, ret)
+		elseif type(v) == "number" then
+			ret[#ret + 1] = v
+		end
+	end
+end
+
 function M.setup(opts)
 	if opts.emoji_path == nil then
 		opts.emoji_path = "/home/jason/drgn_32/"
@@ -36,43 +69,26 @@ function M.setup(opts)
 		opts.emoji_regex = ":drgn_[a-zA-Z0-9_\\-]\\+:"
 	end
 
-	local conceal_cmd = 'syntax match myConceal "' .. opts.emoji_regex .. '" conceal'
-	vim.api.nvim_create_autocmd(
-		{ "VimEnter", "BufWritePost", "WinScrolled" },
-		{ -- FIXME: stuff gets weird with WinScrolled; must be because we call move_cursor inside here
-			callback = function(ev)
-				-- print(vim.inspect(ev))
-				if ev.event == "WinScrolled" then
-					if vim.v.event.all.height ~= 0 or vim.v.event.all.width ~= 0 then
-						return
-					end
-					-- print(vim.inspect(vim.v.event))
+	vim.api.nvim_create_autocmd({ "VimEnter", "BufWritePost", "WinScrolled" }, {
+		callback = function(ev)
+			-- print(vim.inspect(ev))
+			if ev.event == "WinScrolled" then
+				-- TODO: this is bad
+				if vim.v.event.all.height ~= 0 or vim.v.event.all.width ~= 0 then
+					return
 				end
-				clear_imgs()
-				vim.cmd(conceal_cmd)
-				local first_line = vim.fn.winsaveview().topline
-				local matches = vim.fn.matchbufline(
-					ev.buf,
-					opts.emoji_regex,
-					first_line,
-					first_line + vim.api.nvim_win_get_height(0)
-				)
-				-- print(vim.inspect(matches))
-				for _, match in ipairs(matches) do
-					local text = match.text
-					if string.gmatch(text, "^drgn.*$") then
-						local path = opts.emoji_path .. string.sub(text, 2, #text - 1) .. ".png"
-						-- print(path .. " exists!")
-						local abs_pos = vim.fn.screenpos(0, match.lnum, match.byteidx)
-						move_cursor(abs_pos.row, abs_pos.col)
-						M.show_image(0, 0, path)
-						restore_cursor()
-						-- print("Image sent!")
-					end
-				end
-			end,
-		}
-	)
+				-- print(vim.inspect(vim.v.event))
+			end
+			clear_imgs()
+			-- render_window(0, opts)
+			local windows = vim.fn.winlayout()
+			local window_ids = {}
+			extract_window_ids(windows, window_ids)
+			for i = 1, #window_ids do
+				render_window(window_ids[i], opts)
+			end
+		end,
+	})
 end
 
 return M
